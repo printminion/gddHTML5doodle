ab0d49f0e1cc57dfdbb7ffd18694bb4378e337c1
russenreaktor <cr@russenreaktor.de>
2011-08-11 01:54:10 +0200

    initial checkin

---
+++ trunk/js/doodle.js
@@ -0,0 +1,35 @@
+
+function step1() {
+	/*
+	 * start music
+	 */
+	
+	/*
+	 * map container z rotate => isometric image
+	 */
+	
+	/*
+	 * beat on each cell
+	 */	
+	
+	/*
+	 * show maps pieces
+	 */
+	
+}
+
+function step2() {
+	/*
+	 * transform to 3d
+	 */
+}
+
+function step3() {
+	
+}
+
+function step4() {
+	
+}
+
+
